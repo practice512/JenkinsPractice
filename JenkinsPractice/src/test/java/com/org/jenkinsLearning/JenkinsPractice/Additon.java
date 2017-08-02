7b0f9738515a7454adcc356ecc625da96a066217
Sandeep <sandeep.kancharla@gallop.net>
2017-08-02 16:24:50 +0530

    Added project jenkins project

---
+++ JenkinsPractice/src/test/java/com/org/jenkinsLearning/JenkinsPractice/Additon.java
@@ -0,0 +1,13 @@
+package com.org.jenkinsLearning.JenkinsPractice;
+
+public class Additon {
+
+	public static void main(String[] args) {
+		int a =20;
+		int b=30;
+		
+		System.out.println("Additon is "+(a+b));
+
+	}
+
+}
---
+++ JenkinsPractice/src/test/java/com/org/jenkinsLearning/JenkinsPractice/Factorial.java
@@ -0,0 +1,18 @@
+package com.org.jenkinsLearning.JenkinsPractice;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=5;
+		int x=1;
+		for(i=5;i>=1;i--){						
+	     x= x*i;
+		}
+		
+		System.out.println("Final result is "+x);
+		//int result = 
+
+	}
+
+}
